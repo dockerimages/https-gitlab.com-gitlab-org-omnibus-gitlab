fe27cadb5fc838daf666fd19d0d31373f984f882
Balasankar C <balasankar@gitlab.com>
2017-03-13 12:25:37 +0530

    Add deprovision step

--- support/packer/packer-build.sh
+++ support/packer/packer-build.sh
@@ -8,4 +8,9 @@ PACKER_PATH=$(pwd)/support/packer
 
 cd $PACKER_PATH
 
+# Azure instances needs to be deprovisioned at the end of the build process
+if [ $PROVIDER == "azure" ]; then
+    echo "/usr/sbin/waagent -force -deprovision+user && export HISTSIZE=0 && sync" >> $PACKER_PATH/update-script-$TYPE.sh
+fi
+
 packer build -var "version=$VERSION" "$PACKER_PATH/packer-$PROVIDER-$TYPE.json"
