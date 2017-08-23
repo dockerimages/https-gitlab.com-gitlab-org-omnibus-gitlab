f5f8b959b4a7a465500b79747883dd9bc4a2d188
DJ Mountney <dj@gitlab.com>
2017-08-23 16:11:31 -0700

    Cleanup refernces to generate hash

--- spec/libraries/gitlab_spec.rb
+++ spec/libraries/gitlab_spec.rb
@@ -7,20 +7,20 @@ describe Gitlab do
         expect(Gitlab.attribute('test_attribute')[:parent]).to eq 'gitlab'
       end
       expect(Gitlab['test_attribute']).not_to be_nil
-      expect(Gitlab.generate_hash['gitlab']).to include('test-attribute')
+      expect(Gitlab.hyphenate_config_keys['gitlab']).to include('test-attribute')
     end
   end
 
   it 'sets top level attributes when no parent is provided' do
     Gitlab.attribute('test_attribute')
     expect(Gitlab['test_attribute']).not_to be_nil
-    expect(Gitlab.generate_hash).to include('test-attribute')
+    expect(Gitlab.hyphenate_config_keys).to include('test-attribute')
   end
 
   it 'properly defines roles' do
     Gitlab.role('test_node')
     expect(Gitlab['test_node_role']).not_to be_nil
-    expect(Gitlab.generate_hash['roles']).to include('test-node')
+    expect(Gitlab.hyphenate_config_keys['roles']).to include('test-node')
   end
 
   it 'supports overriding attribute default configuration' do
@@ -33,14 +33,14 @@ describe Gitlab do
     hide_const('GitlabEE')
     expect(Gitlab.ee_attribute('test_attribute')[:enable]).to eq false
     expect(Gitlab['test_attribute']).not_to be_nil
-    expect(Gitlab.generate_hash).not_to include('test-attribute')
+    expect(Gitlab.hyphenate_config_keys).not_to include('test-attribute')
   end
 
   it 'enables ee attributes when EE is enabled' do
     stub_const('GitlabEE', 'constant')
     expect(Gitlab.ee_attribute('test_attribute')[:enable]).to eq true
     expect(Gitlab['test_attribute']).not_to be_nil
-    expect(Gitlab.generate_hash).to include('test-attribute')
+    expect(Gitlab.hyphenate_config_keys).to include('test-attribute')
   end
 
   it 'sorts attributes by sequence' do
