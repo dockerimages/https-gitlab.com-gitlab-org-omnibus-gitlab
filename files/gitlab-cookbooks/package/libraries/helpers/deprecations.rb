39b77834c52fd47404b77e8aa8a3336e94280d3d
Balasankar "Balu" C <balasankar@gitlab.com>
2017-12-18 17:16:57 +0530

    Add comment

--- files/gitlab-cookbooks/package/libraries/helpers/deprecations.rb
+++ files/gitlab-cookbooks/package/libraries/helpers/deprecations.rb
@@ -1,6 +1,7 @@
 class Deprecations
   def self.deprecated_settings
     settings = [
+      # Format is Old category, Old Setting, New Category, New Setting, Docs URL
       %w(nginx listen_address nginx listen_addresses)
     ]
     settings
