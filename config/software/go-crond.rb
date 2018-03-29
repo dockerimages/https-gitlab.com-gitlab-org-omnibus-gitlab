9ccf0526e416e2fab681508960ed24169919c5dd
Richard Clamp <richardc@unixbeard.net>
2018-03-29 18:00:54 +0100

    Install go-crond

---
+++ config/software/go-crond.rb
@@ -0,0 +1,35 @@
+#
+# Copyright 2018 GitLab
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+name 'go-crond'
+default_version '0.6.1'
+
+license 'BSD-2-Clause'
+license_file 'LICENSE'
+
+source git: "https://github.com/webdevops/go-crond"
+
+relative_path 'src/github.com/webdevops/go-crond'
+
+build do
+  env = {
+    'GOPATH' => "#{Omnibus::Config.source_dir}/go-crond",
+  }
+  # TODO add a glide/dep lock - currently this just pulls deps from random heads
+  command "go get", env: env
+  command 'go build', env: env
+  copy 'go-crond', "#{install_dir}/embedded/bin/"
+end
