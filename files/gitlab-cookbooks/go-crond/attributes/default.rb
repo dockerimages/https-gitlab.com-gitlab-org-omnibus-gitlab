a3733507adef67768a55463a30ad57a4b0295390
Richard Clamp <richardc@unixbeard.net>
2018-04-03 11:28:03 +0100

    Basic go-crond cookbook

---
+++ files/gitlab-cookbooks/go-crond/attributes/default.rb
@@ -0,0 +1,2 @@
+default['go-crond']['log_directory'] = '/var/log/gitlab/go-crond'
+default['go-crond']['cron_d'] = '/opt/gitlab/embedded/etc/cron.d'
---
+++ files/gitlab-cookbooks/go-crond/recipes/enable.rb
@@ -0,0 +1,15 @@
+directory node["go-crond"]["cron_d"] do
+  recursive true
+  owner "root"
+end
+
+runit_service "go-crond" do
+  owner "root"
+  group "root"
+  options(
+    {
+      log_directory: node["go-crond"]["log_directory"],
+      cron_d: node["go-crond"]["cron_d"]
+    })
+  log_options node["go-crond"].to_hash
+end
