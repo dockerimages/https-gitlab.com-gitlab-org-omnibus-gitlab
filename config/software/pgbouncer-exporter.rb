208c52a7912b055a1e26be1b3e42f802bba691e2
Stan Hu <stanhu@gmail.com>
2018-04-29 20:35:15 -0700

    Don't download .libs to prevent Python Wheels from downloading dependencies

--- config/software/pgbouncer-exporter.rb
+++ config/software/pgbouncer-exporter.rb
@@ -28,7 +28,7 @@ dependency 'python3'
 build do
   env = with_standard_compiler_flags(with_embedded_path)
   command "#{install_dir}/embedded/bin/pip3 install --upgrade setuptools"
-  command "#{install_dir}/embedded/bin/pip3 install prometheus-pgbouncer-exporter==#{version}", env: env
+  command "#{install_dir}/embedded/bin/pip3 install --only-binary=:all: prometheus-pgbouncer-exporter==#{version}", env: env
   command "find #{install_dir}/embedded/lib/python3.4 -name '*.dist-info' -type d -print -exec rm -r {} +"
   command "find #{install_dir}/embedded/lib/python3.4 -name '*.egg-info' -type d -print -exec rm -r {} +"
   command "find #{install_dir}/embedded/lib/python3.4 -name '__pycache__' -type d -print -exec rm -r {} +"
