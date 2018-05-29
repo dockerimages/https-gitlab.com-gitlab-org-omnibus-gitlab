c38d1ffb266ad8b3ebfa1fa4b724e8e7b14982b8
Ben Kochie <bjk@gitlab.com>
2018-05-29 11:50:38 +0200

    Add the Prometheus storage migration tool.
    Add the Promtheus storage migration tool[0] for use with Prometheus 1.x
    to 2.x upgrades.
    
    [0]: https://gitlab.com/gitlab-org/prometheus-storage-migrator

---
+++ config/software/prometheus-storage-migrator.rb
@@ -0,0 +1,41 @@
+#
+## Copyright:: Copyright (c) 2014 GitLab.com
+## License:: Apache License, Version 2.0
+##
+## Licensed under the Apache License, Version 2.0 (the "License");
+## you may not use this file except in compliance with the License.
+## You may obtain a copy of the License at
+##
+## http://www.apache.org/licenses/LICENSE-2.0
+##
+## Unless required by applicable law or agreed to in writing, software
+## distributed under the License is distributed on an "AS IS" BASIS,
+## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+## See the License for the specific language governing permissions and
+## limitations under the License.
+##
+#
+
+require "#{Omnibus::Config.project_root}/lib/gitlab/version"
+
+name 'prometheus'
+version = Gitlab::Version.new('prometheus-storage-migrator')
+default_version version.print
+
+license 'APACHE-2.0'
+license_file 'LICENSE'
+
+source git: version.remote
+
+relative_path 'src/gitlab.com/gitlab-org/prometheus-storage-migrator'
+
+build do
+  env = {
+    'GOPATH' => "#{Omnibus::Config.source_dir}/prometheus-storage-migrator",
+  }
+  source_dir = "#{Omnibus::Config.source_dir}/prometheus-storage-migrator"
+  cwd = "#{source_dir}/#{relative_path}"
+
+  command 'go build', env: env, cwd: cwd
+  copy 'prometheus-storage-migrator', "#{install_dir}/embedded/bin/"
+end
