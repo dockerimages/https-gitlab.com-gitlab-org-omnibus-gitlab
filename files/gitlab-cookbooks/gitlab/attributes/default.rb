5ae37a6e19a17e383a485e6945671788be60590c
abuerer <abuerer@gitlab.com>
2018-06-01 15:15:38 -0500

    WIP: change postgres-exporter sslmode from allow to require

--- files/gitlab-cookbooks/gitlab/attributes/default.rb
+++ files/gitlab-cookbooks/gitlab/attributes/default.rb
@@ -817,7 +817,7 @@ default['gitlab']['postgres-exporter']['home'] = '/var/opt/gitlab/postgres-expor
 default['gitlab']['postgres-exporter']['log_directory'] = "/var/log/gitlab/postgres-exporter"
 default['gitlab']['postgres-exporter']['listen_address'] = 'localhost:9187'
 default['gitlab']['postgres-exporter']['env'] = {
-  'DATA_SOURCE_NAME' => "user=#{node['gitlab']['postgresql']['username']} host=#{node['gitlab']['gitlab-rails']['db_host']} database=postgres sslmode=allow"
+  'DATA_SOURCE_NAME' => "user=#{node['gitlab']['postgresql']['username']} host=#{node['gitlab']['gitlab-rails']['db_host']} database=postgres sslmode=require"
 }
 
 ####
