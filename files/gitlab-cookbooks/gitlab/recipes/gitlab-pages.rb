c08062d8614c9c4c464491e43b862ddd1dd6a270
Balasankar "Balu" C <balasankar@gitlab.com>
2019-01-26 00:33:43 +0530

    Cleanup sv config files of gitlab-pages

--- files/gitlab-cookbooks/gitlab/recipes/gitlab-pages.rb
+++ files/gitlab-cookbooks/gitlab/recipes/gitlab-pages.rb
@@ -65,6 +65,37 @@ end
 
 runit_service 'gitlab-pages' do
   options({
+    dir: node['gitlab']['gitlab-pages']['dir'],
+    external_http: node['gitlab']['gitlab-pages']['external_http'],
+    external_https: node['gitlab']['gitlab-pages']['external_https'],
+    listen_proxy: node['gitlab']['gitlab-pages']['listen_proxy'],
+    cert: node['gitlab']['gitlab-pages']['cert'],
+    cert_key: node['gitlab']['gitlab-pages']['cert_key'],
+    metrics_address: node['gitlab']['gitlab-pages']['metrics_address'],
+    username: node['gitlab']['user']['username'],
+    inplace_chroot: node['gitlab']['gitlab-pages']['inplace_chroot'],
+    domain: node['gitlab']['gitlab-pages']['domain'],
+    pages_root: node['gitlab']['gitlab-pages']['pages_root'],
+    status_uri: node['gitlab']['gitlab-pages']['status_uri'],
+    max_connections: node['gitlab']['gitlab-pages']['max_connections'],
+    log_format: node['gitlab']['gitlab-pages']['log_format'],
+    log_verbose: node['gitlab']['gitlab-pages']['log_verbose'],
+    redirect_http: node['gitlab']['gitlab-pages']['redirect_http'],
+    use_http2: node['gitlab']['gitlab-pages']['use_http2'],
+    artifacts_server: node['gitlab']['gitlab-pages']['artifacts_server'],
+    artifacts_server_url: node['gitlab']['gitlab-pages']['artifacts_server_url'],
+    artifacts_server_timeout: node['gitlab']['gitlab-pages']['artifacts_server_timeout'],
+    access_control: node['gitlab']['gitlab-pages']['access_control'],
+    gitlab_id: node['gitlab']['gitlab-pages']['gitlab_id'],
+    gitlab_secret: node['gitlab']['gitlab-pages']['gitlab_secret'],
+    auth_redirect_uri: node['gitlab']['gitlab-pages']['auth_redirect_uri'],
+    auth_server: node['gitlab']['gitlab-pages']['auth_server'],
+    auth_secret: node['gitlab']['gitlab-pages']['auth_secret'],
+    admin_https_cert: node['gitlab']['gitlab-pages']['admin_https_cert'],
+    admin_https_key: node['gitlab']['gitlab-pages']['admin_https_key'],
+    admin_https_listener: node['gitlab']['gitlab-pages']['admin_https_listener'],
+    admin_secret_path: File.join(node['gitlab']['gitlab-pages']['dir'], 'admin.secret'),
+    admin_unix_listener: File.join(node['gitlab']['gitlab-pages']['dir'], 'admin.socket'),
     log_directory: log_directory
   }.merge(params))
   log_options node['gitlab']['logging'].to_hash.merge(node['gitlab']['gitlab-pages'].to_hash)
