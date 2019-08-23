d95efdc95c3a197a36d0034204b8d6bc4d034c54
Adrien Kohlbecker <adrien.kohlbecker@gmail.com>
2019-08-23 16:56:34 +0200

    Fix rubocop

--- files/gitlab-cookbooks/gitlab/libraries/gitlab_exporter.rb
+++ files/gitlab-cookbooks/gitlab/libraries/gitlab_exporter.rb
@@ -27,12 +27,12 @@ module GitlabExporter
     # The `gitlab_monitor` settings entry is deprecated and will be removed in Gitlab 13.0
     # Remove this method when preparing this release
     def convert_legacy_settings
-      if Gitlab.key?(:gitlab_monitor)
-        Gitlab[:gitlab_monitor].keys.each do |key|
-          Gitlab[:gitlab_exporter][key] = Gitlab[:gitlab_monitor][key] unless Gitlab[:gitlab_exporter].key?(key)
-        end
-        Gitlab.delete(:gitlab_monitor)
+      return unless Gitlab.key?(:gitlab_monitor)
+
+      Gitlab[:gitlab_monitor].keys.each do |key|
+        Gitlab[:gitlab_exporter][key] = Gitlab[:gitlab_monitor][key] unless Gitlab[:gitlab_exporter].key?(key)
       end
+      Gitlab.delete(:gitlab_monitor)
     end
 
     def parse_gitlab_exporter_settings
