6615bdb51c9a6f152d6d8c5e753abbadda285343
Stan Hu <stanhu@gmail.com>
2019-09-05 02:38:09 -0700

    Bump RubyGems v3.0.6
    This may help resolve slow `gem install` times:
    https://github.com/rubygems/rubygems/issues/2017

--- config/software/rubygems.rb
+++ config/software/rubygems.rb
@@ -15,7 +15,7 @@
 #
 
 name 'rubygems'
-default_version '2.7.9'
+default_version '3.0.6'
 
 license 'MIT'
 license_file 'LICENSE.txt'
@@ -39,7 +39,8 @@ if version && !source
     '2.4.4' => '440a89ad6a3b1b7a69b034233cc4658e',
     '2.4.5' => '5918319a439c33ac75fbbad7fd60749d',
     '2.4.8' => 'dc77b51449dffe5b31776bff826bf559',
-    '2.7.9' => '173272ed55405caf7f858b6981fff526'
+    '2.7.9' => '173272ed55405caf7f858b6981fff526',
+    '3.0.6' => '60d84e843b131fb87c8fd67e8fac6470',
   }
   known_tarballs.each do |version, md5|
     version version do
