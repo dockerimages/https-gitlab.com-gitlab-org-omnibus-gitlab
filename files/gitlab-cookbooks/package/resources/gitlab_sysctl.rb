895bc95a61687d2a1d2faa47ac4ff8b27b04567e
DJ Mountney <dj@gitlab.com>
2020-03-30 16:15:18 -0700

    Make it so the reload is only called once

--- files/gitlab-cookbooks/package/resources/gitlab_sysctl.rb
+++ files/gitlab-cookbooks/package/resources/gitlab_sysctl.rb
@@ -34,13 +34,13 @@ action :create do
   file "create /opt/gitlab/embedded/etc/#{conf_name} #{new_resource.name}" do
     path "/opt/gitlab/embedded/etc/#{conf_name}"
     content "#{new_resource.name} = #{new_resource.value}\n"
-    notifies :run, "execute[load sysctl conf #{new_resource.name}]", :immediately
+    notifies :run, "execute[load sysctl conf #{new_resource.name}]"
     notifies :run, "execute[reload all sysctl conf]"
   end
 
   link "/etc/sysctl.d/#{conf_name}" do
     to "/opt/gitlab/embedded/etc/#{conf_name}"
-    notifies :run, "execute[load sysctl conf #{new_resource.name}]", :immediately
+    notifies :run, "execute[load sysctl conf #{new_resource.name}]"
     notifies :run, "execute[reload all sysctl conf]"
   end
 
