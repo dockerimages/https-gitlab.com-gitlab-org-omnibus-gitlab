48de8a987b317f4c51e2d1f2d31d35cabfd90046
Ian Baum <ibaum@gitlab.com>
2020-04-20 11:28:58 -0500

    Copy over changed files from runit cookbook upgrade
    https://gitlab.com/gitlab-org/build/omnibus-mirror/runit-cookbook/-/commit/4cb15827156d6809f9f83a785fb6314f08508d88

--- files/gitlab-cookbooks/runit/metadata.rb
+++ files/gitlab-cookbooks/runit/metadata.rb
@@ -3,10 +3,7 @@ maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license 'Apache-2.0'
 description 'Installs runit and provides runit_service resource'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '4.3.0'
-
-recipe 'runit', 'Installs and configures runit'
+version '5.1.3'
 
 %w(ubuntu debian centos redhat amazon scientific oracle enterpriseenterprise zlinux).each do |os|
   supports os
@@ -14,4 +11,4 @@ end
 
 source_url 'https://github.com/chef-cookbooks/runit'
 issues_url 'https://github.com/chef-cookbooks/runit/issues'
-chef_version '>= 12.1' if respond_to?(:chef_version)
+chef_version '>= 14.0'
