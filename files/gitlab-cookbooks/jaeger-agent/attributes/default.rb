b3792baadcbbdd7472d28c07d39a94f936a37920
Igor Wiedler <iwiedler@gitlab.com>
2020-04-23 11:50:23 +0200

    service based dir and log_directory

--- files/gitlab-cookbooks/jaeger-agent/attributes/default.rb
+++ files/gitlab-cookbooks/jaeger-agent/attributes/default.rb
@@ -1,7 +1,7 @@
 default['jaeger-agent']['enable'] = false
 default['jaeger-agent']['collector'] = nil
 default['jaeger-agent']['tags'] = nil
-default['jaeger-agent']['dir'] = '/var/opt/gitlab/jaeger/agent'
+default['jaeger-agent']['dir'] = '/var/opt/gitlab/jaeger-agent'
 default['jaeger-agent']['username'] = 'gitlab-jaeger'
 default['jaeger-agent']['group'] = 'gitlab-jaeger'
-default['jaeger-agent']['log_directory'] = '/var/log/gitlab/jaeger/agent'
+default['jaeger-agent']['log_directory'] = '/var/log/gitlab/jaeger-agent'
