cf7e4b75757a5c8357e9c008052817f409551589
Ian Baum <ibaum@gitlab.com>
2020-06-15 20:25:37 -0500

    Add message body to error output on trigger failure
    * Actual API error is in the message body, the HTTP status is less
      important

--- lib/gitlab/build/trigger.rb
+++ lib/gitlab/build/trigger.rb
@@ -22,7 +22,7 @@ module Build
 
       case response
       when Net::HTTPClientError, Net::HTTPServerError
-        raise "Trigger failed! The response from the trigger is: #{response}: #{response.message}"
+        raise "Trigger failed! The response from the trigger is: #{response}: #{response.message} - #{response.read_body}"
       else
         Build::Trigger::CommitComment.post!(pipeline_url, get_access_token) if post_comment
 
