d95a2214d0f738de01433e8afc6399b9e51ad81a
DJ Mountney <dj@gitlab.com>
2021-04-30 19:17:33 -0700

    Fix rails typo

--- config/software/gitlab-rails.rb
+++ config/software/gitlab-rails.rb
@@ -84,7 +84,7 @@ build do
 
   bundle_without = %w(development test)
   bundle_without << 'mysql'
-  bundle 'config build.rugged -use-system-libraries', env: env
+  bundle 'config build.rugged --use-system-libraries', env: env
   bundle 'config build.gpgme --use-system-libraries', env: env
   bundle "config build.nokogiri --use-system-libraries --with-xml2-include=#{install_dir}/embedded/include/libxml2 --with-xslt-include=#{install_dir}/embedded/include/libxslt", env: env
   bundle 'config build.grpc --with-ldflags="-latomic"', env: env if OhaiHelper.os_platform == 'raspbian'
