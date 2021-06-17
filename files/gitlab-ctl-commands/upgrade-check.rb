67830ead358ffc6ff4c814a0b494ea7ddcad037c
Balasankar "Balu" C <balasankar@gitlab.com>
2021-06-17 10:03:17 +0530

    Fix upgrade check error message to show correct minimum version
    Signed-off-by: Balasankar "Balu" C <balasankar@gitlab.com>

--- files/gitlab-ctl-commands/upgrade-check.rb
+++ files/gitlab-ctl-commands/upgrade-check.rb
@@ -3,9 +3,11 @@ require "#{base_path}/embedded/service/omnibus-ctl/lib/gitlab_ctl/upgrade_check"
 add_command('upgrade-check', 'Check if the upgrade is acceptable', 2) do
   old_version = ARGV[3]
   new_version = ARGV[4]
+  min_version = ENV['MIN_VERSION'] || GitlabCtl::UpgradeCheck::MIN_VERSION
+
   unless GitlabCtl::UpgradeCheck.valid?(old_version, new_version)
     warn "It seems you are upgrading from major version #{old_version.split('.').first} to major version #{new_version.split('.').first}."
-    warn "It is required to upgrade to the latest #{GitlabCtl::UpgradeCheck::MIN_VERSION}.x version first before proceeding."
+    warn "It is required to upgrade to the latest #{min_version}.x version first before proceeding."
     warn "Please follow the upgrade documentation at https://docs.gitlab.com/ee/update/index.html#upgrading-to-a-new-major-version"
     Kernel.exit 1
   end
